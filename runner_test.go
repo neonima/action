8700c8e4ed9c9b0408ec6e3c91698b3a88a6094f
Vincent Gschwend <12810603+neonima@users.noreply.github.com>
2025-05-11 10:04:47 +0200

    Add hook so that after each action a process can be started

--- runner_test.go
+++ runner_test.go
@@ -2,6 +2,7 @@ package action_test
 
 import (
 	"context"
+	"errors"
 	"github.com/neonima/action"
 	"github.com/stretchr/testify/require"
 	"testing"
@@ -35,13 +36,59 @@ func TestRunner_Start(t *testing.T) {
 	})
 }
 
-func TestRunner_WaitStopped(t *testing.T) {
-	t.Run("Should return an error when the context is canceled", func(t *testing.T) {
+func TestRunner_Done(t *testing.T) {
+	t.Run("Should return a channel closed when the runner is stopped", func(t *testing.T) {
 		r := action.New()
 		ctx, cancel := context.WithCancel(t.Context())
-		t.Cleanup(cancel)
-		require.NoError(t, r.Start(ctx))
+		err := r.Start(ctx)
+		require.NoError(t, err)
 		cancel()
-		require.ErrorIs(t, r.WaitStopped(), context.Canceled)
+		<-r.Done()
+		require.Error(t, r.Error())
+	})
+}
+
+func TestRunner_Error(t *testing.T) {
+	t.Run("Should return the error of the runner", func(t *testing.T) {
+		r := action.New()
+		ctx, cancel := context.WithCancel(t.Context())
+		err := r.Start(ctx)
+		require.NoError(t, err)
+		cancel()
+	})
+}
+
+func TestRunner_WithHook(t *testing.T) {
+	t.Run("Should add a hook to the runner", func(t *testing.T) {
+		incr := 0
+		r := action.New(action.WithHook(func(ctx context.Context) error {
+			incr++
+
+			return nil
+		}))
+		ctx, cancel := context.WithCancel(t.Context())
+		require.NoError(t, r.Start(ctx))
+		action.Act(r, func() {
+			incr--
+		})
+		cancel()
+		<-r.Done()
+		require.Error(t, r.Error())
+		require.Equal(t, 0, incr)
+	})
+	t.Run("Should add a hook to the runner and stop the execution if it errored", func(t *testing.T) {
+		incr := 0
+		r := action.New(action.WithHook(func(ctx context.Context) error {
+			return errors.New("error")
+		}))
+		ctx, cancel := context.WithCancel(t.Context())
+		require.NoError(t, r.Start(ctx))
+		action.Act(r, func() {
+			incr--
+		})
+		cancel()
+		<-r.Done()
+		require.Error(t, r.Error())
+		require.Equal(t, -1, incr)
 	})
 }
