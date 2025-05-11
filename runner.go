02f702b82a23dbbeeb832610d53c24fc6df32460
Vincent Gschwend <12810603+neonima@users.noreply.github.com>
2025-05-11 10:04:47 +0200

    Add hook so that after each action a process can be started

--- runner.go
+++ runner.go
@@ -2,6 +2,7 @@ package action
 
 import (
 	"context"
+	"sync"
 	"sync/atomic"
 )
 
@@ -15,7 +16,10 @@ type Runner struct {
 	stream    chan Action
 	isStarted atomic.Bool
 	ctx       context.Context
-	errCh     chan error
+	hooks     []func(context.Context) error
+	done      chan struct{}
+	err       atomic.Pointer[error]
+	sync.Once
 }
 
 // New returns a new Runner with default configuration settings.
@@ -25,7 +29,7 @@ type Runner struct {
 func New(opts ...func(*Runner)) *Runner {
 	r := &Runner{
 		stream: make(chan Action, 1),
-		errCh:  make(chan error, 1),
+		done:   make(chan struct{}, 1),
 	}
 
 	for _, opt := range opts {
@@ -46,6 +50,19 @@ func WithChanSize(size int) func(*Runner) {
 	}
 }
 
+// WithHook adds a hook to the runner.
+// The hook will be called after each action is executed.
+// The hook can be used to perform some cleanup or logging.
+func WithHook(h func(ctx context.Context) error) func(*Runner) {
+	return func(r *Runner) {
+		if h == nil {
+			return
+		}
+
+		r.hooks = append(r.hooks, h)
+	}
+}
+
 // Start starts the runner on a separated goroutine
 func (r *Runner) Start(ctx context.Context) error {
 	if r.isStarted.Load() {
@@ -61,25 +78,45 @@ func (r *Runner) Start(ctx context.Context) error {
 }
 
 func (r *Runner) start(ctx context.Context) {
+	defer func() {
+		r.Once.Do(func() {
+			close(r.stream)
+			close(r.done)
+		})
+	}()
 	for {
 		select {
 		case <-ctx.Done():
-			close(r.stream)
-			r.errCh <- ctx.Err()
+			err := ctx.Err()
+			r.err.Store(&err)
 			return
 		case action, ok := <-r.stream:
 			if !ok {
 				return
 			}
 			action()
+			for _, h := range r.hooks {
+				if err := h(ctx); err != nil {
+					r.err.Store(&err)
+					return
+				}
+			}
 		}
 	}
 }
 
-// WaitStopped blocks until the runner shuts down
-// and returns the shutdown reason, typically context.Canceled.
-func (r *Runner) WaitStopped() error {
-	return <-r.errCh
+// Done returns a channel closed when the runner is stopped.
+func (r *Runner) Done() <-chan struct{} {
+	return r.done
+}
+
+// Err returns the error of the runner. To be used with Done()
+func (r *Runner) Error() error {
+	errPtr := r.err.Load()
+	if errPtr == nil {
+		return nil
+	}
+	return *errPtr
 }
 
 // Send enqueues an action onto the actor's queue.
